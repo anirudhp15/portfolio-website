9e9e4614cfcf736f417d4f712d179e0f3a521ae5
Ani Pottammal <anirudhpottammal@nyu.edu>
2023-12-07 18:47:53 -0500

    Add files via upload
    12/7/23

--- app.js
+++ app.js
@@ -8,5 +8,15 @@ const observer = new IntersectionObserver(entries => {
     }
   });
 });
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+const elements = document.querySelectorAll('.hidden, .header');
+elements.forEach((el) => observer.observe(el));
+
+window.onload = function() {
+  setTimeout(function() {
+      const delayedText = document.querySelector('.delayed-text');
+      const navItems = document.querySelectorAll('.nav-item');
+      navItems.forEach((item) => item.classList.add('nav-item-visible'));
+      delayedText.style.visibility = 'visible';
+      delayedText.classList.add('visible');
+  }, 1000); // Delay of 1 second
+};
